5dbf082638d077c6388c41440800d35fffe92cbe
nik-kita <nik-kita.deno.dev>
2024-03-30 03:03:47 +0100

    /all

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from sqlmodel import select
+
+from src.database.db import ActualSession
+from src.database.user import User
 from .routers.auth import Me, router as auth_router
 from contextlib import asynccontextmanager
 from os import system
@@ -18,3 +22,10 @@ app.include_router(auth_router, prefix="/auth", tags=["auth"])
 @app.get("/me")
 async def get_me(me: Me):
     return me
+
+
+@app.get("/all")
+async def get_all(me: Me, session: ActualSession):
+    users = session.exec(select(User).limit(100)).all()
+
+    return [{"email": u.email} for u in users]
