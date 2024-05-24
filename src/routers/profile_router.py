569d7ae0f6e5a1cf80e4327fb3846cd5f18ff32a
nik-kita <nik-kita.deno.dev>
2024-05-24 02:23:33 +0200

    Refactor profile_router.py to include new endpoints for retrieving profiles by name or nik

--- src/routers/profile_router.py
+++ src/routers/profile_router.py
@@ -1,25 +1,60 @@
 from fastapi import APIRouter, Response, status
-from common.auth import Me_and_Session
+from fastapi.responses import JSONResponse
+from common.auth import Me_and_Session, MeOptional_and_Session
+from schemas.exception_400_schema import Exception_400
 from src.services import profile_service
 from schemas.profile_schema import ModifyProfileReq, PaginatedRes, ProfileRes, ProfileReq
-
+from src.services import user_service
 
 profile_router = APIRouter()
 
 
-@profile_router.get('/{name}')
-def get_profile_by_name(name: str, me_and_session: Me_and_Session) -> ProfileRes | None:
+@profile_router.get('/{nik}/{profile_name}', responses={400: {"model": Exception_400}})
+def get_profile_by_name(nik: str, profile_name: str, me_and_session: MeOptional_and_Session):
     me, session = me_and_session
+
+    if me.nik == nik:
+        res = profile_service.get(
+            user_id=me.id, profile_name=profile_name, session=session)
+
+        return res
+
+    user = user_service.get_by_nik(user_nik=nik, session=session)
+
+    if not user:
+        return JSONResponse(status_code=400, content=Exception_400(message=f"User '{nik}' not found").model_dump())
+
     res = profile_service.get(
-        user_id=me.id, profile_name=name, session=session)
+        user_id=user.id, profile_name=profile_name, session=session)
 
     return res
 
 
+@profile_router.get('/{profile_name_or_nik}')
+# for user get 1 profile by name
+# for guest get all (user by nik)'s profiles
+def get_1_profile_by_name_or_many_of_user_by_nik(profile_name_or_nik: str, me_and_session: MeOptional_and_Session) -> PaginatedRes[ProfileRes] | ProfileRes | None:
+    me, session = me_and_session
+
+    if not me:
+        nik = profile_name_or_nik
+        res = profile_service.get_all_public(nik=nik, session=session)
+
+        return res
+
+    else:
+        profile_name = profile_name_or_nik
+        res = profile_service.get(
+            user_id=me.id, profile_name=profile_name, session=session)
+
+        return res
+
+
 @profile_router.get('/')
+# get all own profiles
 def get_all_profiles(me_and_session: Me_and_Session) -> PaginatedRes[ProfileRes]:
     me, session = me_and_session
-    res = profile_service.get_all(user_id=me.id, session=session)
+    res = profile_service.get_all_own(user_id=me.id, session=session)
 
     return res
 
